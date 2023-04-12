f31ac00b40b78ba22c23498e2f628f89d1bbd1e4
EmmyC <esc@Emilys-MBP-2.attlocal.net>
2023-04-12 17:09:34 -0500

    BalancedBracketsTesting initial commit - green play arrow issues fixed

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -11,11 +11,10 @@ import main.BalancedBrackets;
 public class BalancedBracketsTest {
 
     //TODO: add tests here
-        //@Test
-        //public void emptyTest() {
+    @Test
+    public void emptyTest() {
         assertEquals(true, true);
     }
-        //}
 
 //POSITIVE TEST CASES//
 
@@ -25,7 +24,7 @@ public class BalancedBracketsTest {
         String spec = "a single pair of brackets returns true";
         String expectedTestData = "[]";
         boolean actualResult = BalancedBrackets.hasBalancedBrackets(expectedTestData);
-        assertTrue(spec, actualResult));
+        assertTrue(spec, actualResult);
     }
 
     //balanced brackets in front of other characters --> "[]LaunchCode"
@@ -34,7 +33,7 @@ public class BalancedBracketsTest {
         String spec = "a pair of brackets before other characters returns true";
         String expectedTestData = "[]LaunchCode";
         boolean actualResult = BalancedBrackets.hasBalancedBrackets(expectedTestData);
-        assertTrue(spec, actualResult));
+        assertTrue(spec, actualResult);
     }
 
     //balanced brackets around other characters --> "[LaunchCode]"
@@ -43,7 +42,7 @@ public class BalancedBracketsTest {
         String spec = "a pair of brackets around other characters returns true";
         String expectedTestData = "[LaunchCode]";
         boolean actualResult = BalancedBrackets.hasBalancedBrackets(expectedTestData);
-        assertTrue(spec, actualResult));
+        assertTrue(spec, actualResult);
     }
 
     //"Launch[Code]"
@@ -52,7 +51,7 @@ public class BalancedBracketsTest {
         String spec = "a pair of brackets within/among other characters returns true";
         String expectedTestData = "Launch[Code]";
         boolean actualResult = BalancedBrackets.hasBalancedBrackets(expectedTestData);
-        assertTrue(spec, actualResult));
+        assertTrue(spec, actualResult);
     }
 
     //"[]"
@@ -61,7 +60,7 @@ public class BalancedBracketsTest {
         String spec = "a single pair of brackets returns true";
         String expectedTestData = "[]";
         boolean actualResult = BalancedBrackets.hasBalancedBrackets(expectedTestData);
-        assertTrue(spec, actualResult));
+        assertTrue(spec, actualResult);
     }
 
     //nested pairs of brackets [[]];
@@ -70,17 +69,17 @@ public class BalancedBracketsTest {
         String spec = "nested pairs of balanced brackets returns true";
         String expectedTestData = "[[]]";
         boolean actualResult = BalancedBrackets.hasBalancedBrackets(expectedTestData);
-        assertTrue(spec, actualResult));
+        assertTrue(spec, actualResult);
     }
 
-//NEGATIVE TEST CASES//
+    //NEGATIVE TEST CASES//
     //single opening bracket --> "[";
     @Test
     public void singleOpeningBracket() {
         String spec = "a single opening bracket returns false";
         String expectedTestData = "[";
         boolean actualResult = BalancedBrackets.hasBalancedBrackets(expectedTestData);
-        assertFalse(spec, actualResult));
+        assertFalse(spec, actualResult);
     }
 
     //single closing bracket --> "]";
@@ -89,7 +88,7 @@ public class BalancedBracketsTest {
         String spec = "a single closing bracket returns false";
         String expectedTestData = "]";
         boolean actualResult = BalancedBrackets.hasBalancedBrackets(expectedTestData);
-        assertFalse(spec, actualResult));
+        assertFalse(spec, actualResult);
     }
 
     //pair of reversed brackets --> "][";
@@ -98,7 +97,7 @@ public class BalancedBracketsTest {
         String spec = "a pair of reversed brackets returns false";
         String expectedTestData = "][";
         boolean actualResult = BalancedBrackets.hasBalancedBrackets(expectedTestData);
-        assertFalse(spec, actualResult));
+        assertFalse(spec, actualResult);
     }
 
     //single bracket with other characters --> "[LaunchCode";
@@ -107,7 +106,7 @@ public class BalancedBracketsTest {
         String spec = "a single bracket with other characters returns false";
         String expectedTestData = "[LaunchCode";
         boolean actualResult = BalancedBrackets.hasBalancedBrackets(expectedTestData);
-        assertFalse(spec, actualResult));
+        assertFalse(spec, actualResult);
     }
 
     //unbalanced brackets whithin/among other characters --> "Launch]Code["
@@ -116,14 +115,14 @@ public class BalancedBracketsTest {
     //a series of mixed, unbalanced, brackets --> "[[][[]]]["
 
 
-//EDGE CASES//
+    //EDGE CASES//
     //empty string --> ""
     @Test
     public void emptyString() {
         String spec = "an empty string returns true";
         String expectedTestData = "";
         boolean actualResult = BalancedBrackets.hasBalancedBrackets(expectedTestData);
-        assertTrue(spec, actualResult));
+        assertTrue(spec, actualResult);
     }
 
     //string with only characters, no brackets --> "LaunchCode"
@@ -132,8 +131,8 @@ public class BalancedBracketsTest {
         String spec = "a string with only characters, no brackets, returns true";
         String expectedTestData = "LaunchCode";
         boolean actualResult = BalancedBrackets.hasBalancedBrackets(expectedTestData);
-        assertTrue(spec, actualResult));
+        assertTrue(spec, actualResult);
     }
 
-
+} //end BalancedBracketsTest
 
